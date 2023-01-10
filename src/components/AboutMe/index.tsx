a02fc896c26893903d25e49929a765131929e54e
Brandon Fernandez <brandox02.dev@gmail.com>
2023-01-10 13:55:35 -0400

    remove the "1 year experience text"

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -19,7 +19,7 @@ function AboutMe() {
                <div className="col-12 col-md-9">
                     <div style={{ padding: 10 }}>
                          <p style={{ fontSize: 20 }}>
-                              My name is Brandon Saul Fernandez Mejia, I am a young and passionate software developer for 1 year and a half, I really like being able to develop effective, functional and presentable solutions for the different problems of daily life as well as being able to facilitate myself and others life through my knowledge in the world of software.
+                              My name is Brandon Saul Fernandez Mejia, I am a young and passionate software developer, I really like being able to develop effective, functional and presentable solutions for the different problems of daily life as well as being able to facilitate myself and others life through my knowledge in the world of software.
                          </p>
                          <p style={{ fontSize: 20 }}>
                               I am also a very curious person who loves to try different things and always be updated with what surrounds me. in addition to being always active and open to consider and learn new technologies.
